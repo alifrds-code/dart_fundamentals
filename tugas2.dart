5a7f82aedfcdf88ea024df76200edeb38f2378cd
alifrds-code <faisalalifrds@gmail.com>
2026-02-09 09:33:19 +0700

    Tugas  2 Dart

--- tugas2.dart
+++ tugas2.dart
@@ -11,11 +11,12 @@ void main(){
   String? inputNilaiUTS = stdin.readLineSync();
   int nilaiUTS = int.parse(inputNilaiUTS?? "0");
 
-  // Membuat
+  // Membuat input nilai uas
   stdout.write("Masukkan Nilai UAS: ");
   String? inputNilaiUAS = stdin.readLineSync();
   int nilaiUAS = int.parse(inputNilaiUAS?? "0");
   
+  // Membuat input kehadiran
   stdout.write("Masukkan Nilai Kehadiran: ");
   String? inputKehadiran = stdin.readLineSync();
   double kehadiran = double.parse(inputKehadiran?? "0");
@@ -23,10 +24,13 @@ void main(){
   cekKelulusan(nilaiUTS, nilaiUAS, kehadiran);
   
 } 
+// Membuat fungsi cek kelulusan
 void cekKelulusan(int nilaiUTS, int nilaiUAS, double kehadiran){
 
+  // Menghitung nilai rata rata
   double rataRata = (nilaiUTS + nilaiUAS) / 2;
 
+  // cek kondisi lulus
   bool lulus = rataRata >= 70 && kehadiran >= 75 && (nilaiUTS >= 60 || nilaiUAS >= 60);
 
   if (lulus) {
